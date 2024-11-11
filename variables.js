9e2af38c051dcbc4631e4dd671167c2ddac52ef5
Yandile-Matyityilili <matyityililiy@gmail.com>
2024-11-11 11:48:47 +0200

    added new js

---
+++ variables.js
@@ -0,0 +1,70 @@
+//Variables
+//let name = "Yandile"
+//const name = "Yandile"
+
+//let value=true//false
+// undefined
+//let name
+//let value = null
+/*var name="yandile"
+number = 5
+console.log(typeof name)*/
+
+// let name = true
+// let num=5
+// console.log(`My name is ${name}`);
+// console.log("My name is"+ name+num);
+
+/*let myArray=['anything',2,true,'string']
+console.log(myArray[0]); */  ///indexing
+
+//camelcase > myArray(always used in JS)
+// Pascalcase > MyArray
+// kebabcase > my-array(only use in vue)
+// ... cant find what you are referencing
+//object literal - declare (make) an object
+
+// let table = {
+//     material:"wood",
+//     no_of_legs:4, //property name
+//     occupied:true,
+// }
+
+//bracket notation
+// console.log(table['material']);
+// dot notation
+// console.log(table.material);
+// destructuring
+// let{material} = table//wood
+// let{occupied} = table//wood
+// console.log(material);
+// the code above and below is the same
+// let{material,occupied} = table
+// console.log(material);
+//function
+// () > parentheses
+// [] > brackets
+// {} > braces
+// function greeting(a,b){
+    //a&b are parameters
+    // console.log('This is a greeting'+ a+b);
+    
+// }
+// 3&4 are arguments 
+// greeting(3,4)
+
+// let num1=5
+// ++num1 pre increment
+// num1++ post increment
+// --num1 pre decrement
+// num1-- post decrement
+//console.log(--num1);
+// console.log(6=="6")
+
+let myString = "Something"
+// shows us the length on the string
+let length = myString.length
+//make text capital --> myString.toUpperCase()
+//make text small letter --> myString.toLowerCase()
+let split = myString.toLowerCase.split('')
+console.log(split);
